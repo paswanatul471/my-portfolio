5bdcc7c47ee8ffe694e8ef3e155b82747fae023b
Atul Paswan <90141410+paswanatul471@users.noreply.github.com>
2023-09-05 16:09:57 +0530

    updated

--- src/components/Testmonial.jsx
+++ src/components/Testmonial.jsx
@@ -1,35 +1,35 @@
-import React from 'react'
-// import me from "../assets/atul1.png";
+// import React from 'react'
+// // import me from "../assets/atul1.png";
 
-const Testmonial = () => {
-  return (
-    <div id='testimonial'>
-      <h2>Testmonial</h2>
+// const Testmonial = () => {
+//   return (
+//     <div id='testimonial'>
+//       <h2>Testmonial</h2>
       
-      <section>
-        <TestmonialCard name={"Atul"} 
-        feedback={""} 
-        />
-        <TestmonialCard name={"Atul"} 
-        feedback={""} 
-        />
-        <TestmonialCard name={"Atul"} 
-        feedback={""} 
-        />
+//       <section>
+//         <TestmonialCard name={"Atul"} 
+//         feedback={""} 
+//         />
+//         <TestmonialCard name={"Atul"} 
+//         feedback={""} 
+//         />
+//         <TestmonialCard name={"Atul"} 
+//         feedback={""} 
+//         />
 
-      </section>
-      {/* Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat facere, quisquam distinctio illum error laborum, quia qui provident enim dolorem incidunt dolor. Enim iusto ab blanditiis cupiditate id mollitia vel. */}
-    </div>
-  )
-}
+//       </section>
+//       {/* Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat facere, quisquam distinctio illum error laborum, quia qui provident enim dolorem incidunt dolor. Enim iusto ab blanditiis cupiditate id mollitia vel. */}
+//     </div>
+//   )
+// }
 
-const TestmonialCard=({name,feedback})=>(
-<article>
-  <img src="https://cdn-icons-png.flaticon.com/512/149/149071.png" alt="User" />
-  <h4>{name}</h4>
-  <p>{feedback}</p>
+// const TestmonialCard=({name,feedback})=>(
+// <article>
+//   <img src="https://cdn-icons-png.flaticon.com/512/149/149071.png" alt="User" />
+//   <h4>{name}</h4>
+//   <p>{feedback}</p>
 
-</article>
-)
-export default Testmonial
+// </article>
+// )
+// export default Testmonial
 
