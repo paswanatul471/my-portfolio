53e08773d9a7f3788653de2d53f32a05f2e5e558
Atul Paswan <90141410+paswanatul471@users.noreply.github.com>
2023-09-08 08:45:12 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Work from './components/Work'
 import Timeline from './components/Timeline'
 import Services from './components/Services'
 // import Testimonial from './components/Testmonial'
+
 import Contact from './components/Contact'
 import Footer from './components/Footer'
 import { Toaster } from 'react-hot-toast'
